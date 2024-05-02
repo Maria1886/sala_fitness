e2151da65fe91a344bb270bc59003c9336b8659b
Maria1886 <76714446+Maria1886@users.noreply.github.com>
2024-05-02 09:42:16 +0300

    Add files via upload

--- edit_trainers_form.php
+++ edit_trainers_form.php
@@ -1,28 +1,86 @@
 <?php
-require "../database/connection.php";
-$trainers_id=$_GET['trainers_id'];
-$sql="SELECT * FROM `antrenori` WHERE id=$trainers_id ";
-$result = $connection->query($sql);
 
-if ($result->num_rows > 0) {
-    $trainers = $result->fetch_assoc();
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+
+    if (isset($_GET['id'])) {
+        $trainer_id = $_GET['id'];
+        var_dump($trainer_id);
+        require "../database/connection.php";
+    
+        $sql = "SELECT * FROM antrenori  WHERE id=$trainer_id";
+        $result = $connection->query($sql);
+        var_dump($result);
+        if ($result->num_rows > 0) {
+            $antrenor = $result->fetch_assoc();
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+                $nume = $_POST['nume'];
+                $prenume = $_POST['prenume'];
+                $descriere = $_POST['descriere'];
+            
+    
+                if (isset($_FILES['imagine']) && $_FILES['imagine']['error'] === UPLOAD_ERR_OK) {
+                    if (!empty($antrenor['imagine'])) {
+                        $imagePathToDelete = dirname(__FILE__) . '_image_trainers/' . $antrenor['imagine'];
+                        if (file_exists($imagePathToDelete)) {
+                            unlink($imagePathToDelete);
+                        }
+                    }
+    
+                    $directoryPath = dirname(__FILE__);
+                    $directoryName = '_image_trainers';
+    
+                    if (!file_exists($directoryPath . $directoryName)) {
+                        mkdir($directoryPath . $directoryName, 0775, true);
+                        chmod($directoryPath . $directoryName, 0775);
+                    }
+    
+                    $imagePath = $directoryPath . $directoryName . '/' . uniqid() . '_' . basename($_FILES['imagine']['name']);
+                    if (move_uploaded_file($_FILES['imagine']['tmp_name'], $imagePath)) {
+                        $imagePathData = basename($imagePath);
+                    } else {
+                        echo "A apărut o eroare la încărcarea imaginii.";
+                    }
+                } else {
+                    $imagePathData = $antrenor['imagine'];
+                }
+    
+                // Actualizați înregistrarea în baza de date
+                $update_sql = "UPDATE antrenori SET nume='$nume', prenume='$prenume', descriere='$descriere', imagine='$imagePathData' WHERE id=$trainer_id";
+    
+                if ($connection->query($update_sql) === TRUE) {
+                    echo " a fost actualizat cu succes.";
+    
+                    exit;
+                } else {
+                    echo "Eroare la actualizarea ";
+                }
+            }
+    
+            $conn->close();
+        } else {
+            echo " nu a fost găsit.";
+        }
+    }
+
  ?>
  <div class="hide_edit_form">
-        <form action="" method="post" class="register_form" enctype="multipart/form-data">
+        <form method="post" class="register_form" enctype="multipart/form-data">
             <label for="nume">Nume</label>
-            <input type="text" placeholder="numele antrenorului" name="nume" id="nume" value="<?php echo $trainers['nume']?>" required>
+            <input type="text" placeholder="numele antrenorului" name="nume" id="nume" value="<?php echo $trainers['nume']?>" >
             <label for="prenume">Prenume</label>
-            <input type="text" placeholder="prenumele antrenorului" name="prenume" id="prenume" value="<?php echo $trainers['prenume']?>" required>
+            <input type="text" placeholder="prenumele antrenorului" name="prenume" id="prenume" value="<?php echo $trainers['prenume']?>" >
             <label for="descriere">Descriere</label> 
-            <textarea placeholder="descriere" name="descriere" id="descriere" cols="30" rows="10" value="<?php echo $trainers['descriere']?>"></textarea>
+            <textarea placeholder="descriere" name="descriere" id="descriere" cols="30" rows="10"><?php echo $trainers['descriere']?></textarea>
             <label for="imagine">Imagine</label>
-            <input type="file" name="imagine" id="imagine" value="<?php echo $trainers['imagine']?>" required>
+            <input type="file" name="imagine" id="imagine" value="<?php echo $trainers['imagine']?>" >
             <button type="submit" >Editeaza</button>
 
         </form>
         </div>
  <?php
+
 }else {
- echo "Nu s-au găsit antrenori.";
-}
+        echo "Acces interzis.";
+    }
 ?>
